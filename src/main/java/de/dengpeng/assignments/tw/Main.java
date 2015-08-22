42933a4cf67606a5ff38f5ed51948a8af0813ce6
Paul Deng <dengpeng.cn@gmail.com>
2015-08-22 22:47:09 +1000

    Initial project setup

---
+++ src/main/java/de/dengpeng/assignments/tw/Main.java
@@ -0,0 +1,10 @@
+package de.dengpeng.assignments.tw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
