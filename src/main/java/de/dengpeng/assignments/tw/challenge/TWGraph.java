ba137680e22c838cb6f12796d46fbdfc230ed286
Paul Deng <dengpeng.cn@gmail.com>
2015-08-26 12:22:06 +1000

    Source code cleaned up

--- src/main/java/de/dengpeng/assignments/tw/challenge/TWGraph.java
+++ src/main/java/de/dengpeng/assignments/tw/challenge/TWGraph.java
@@ -32,40 +32,6 @@ public class TWGraph {
 			graph.setEdgeWeight(edge, Integer.valueOf(String.valueOf(edgeDef.charAt(2))));
 			
 		}
-		
-		
-//		graph.addVertex("A");
-//		graph.addVertex("B");
-//		graph.addVertex("C");
-//		graph.addVertex("D");
-//		graph.addVertex("E");
-//
-//		DefaultWeightedEdge e1 = graph.addEdge("A", "B");
-//		graph.setEdgeWeight(e1, 5);
-//
-//		DefaultWeightedEdge e2 = graph.addEdge("B", "C");
-//		graph.setEdgeWeight(e2, 4);
-//
-//		DefaultWeightedEdge e3 = graph.addEdge("C", "D");
-//		graph.setEdgeWeight(e3, 8);
-//
-//		DefaultWeightedEdge e4 = graph.addEdge("D", "C");
-//		graph.setEdgeWeight(e4, 8);
-//
-//		DefaultWeightedEdge e5 = graph.addEdge("D", "E");
-//		graph.setEdgeWeight(e5, 6);
-//
-//		DefaultWeightedEdge e6 = graph.addEdge("A", "D");
-//		graph.setEdgeWeight(e6, 5);
-//
-//		DefaultWeightedEdge e7 = graph.addEdge("C", "E");
-//		graph.setEdgeWeight(e7, 2);
-//
-//		DefaultWeightedEdge e8 = graph.addEdge("E", "B");
-//		graph.setEdgeWeight(e8, 3);
-//
-//		DefaultWeightedEdge e9 = graph.addEdge("A", "E");
-//		graph.setEdgeWeight(e9, 7);
 	}
 
 	public int distanceOf(String route) throws Exception {
